c63d9ee6b307396e0ab89b87d4665a53c84a4239
AlidaContreras <alidacontreras01@gmail.com>
2018-05-23 10:28:41 -0500

    Agregue conecciones del encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,4 @@
 window.cipher = {
-  encode:n+m,
-  decode:o+p
-  // ... 
-};
-
-var fruta = {
-  color : 'rojo', 
-  name: 'fresa'
+ encode : (offset, )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-let mensaje=string;
+let offset,mensaje_encode,mensaje_decode,btnEncode,btnDecode; 
+
+function encodeConection() {
+    offset=document.getElementById("txt_offset").value;
+    mensaje_encode=document.getElementById("txt_encode").value;
+    mensaje_decode=document.getElementById("txt_encode").value;
+    btnEncode= document.addEventListener("buttonEncode");
+    function encodeClick() {
+        
+    }
+}
