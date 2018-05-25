f6cee7deeb9572531b67a993b3a9276a2a926c7d
AlidaContreras <alidacontreras01@gmail.com>
2018-05-25 10:19:30 -0500

    Avance un poquito de encode y el resultado cifrado ya sale en resultado

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,24 @@
 window.cipher = {
-    encode:encode,
+    encode: encode,
     decode: decode
 }
 
-function encode() {
-   console.log("hola, soy ENCODE")  
-} 
-function decode(){
-console.log("hola, soy DECODE");
+function encode(offset, mensaje_encode) {
+    let vacio = "";
+    for (let i = 0; i < mensaje_encode.length; i++) {
+        let posicion = mensaje_encode.charCodeAt(i);
+        if ((posicion > 64 && posicion < 91) || (posicion > 96 && posicion < 123)) {
+            vacio += String.fromCharCode(posicion + offset);
+        }
+    document.getElementById("txt_resultado").innerHTML=vacio;
+
+    }
+
+}
+
+function decode() {
+    console.log("hola, soy DECODE");
 
 
 }
+//cipher.encode(2,'ALIDA');
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-let offset,mensaje_encode,mensaje_decode,btnEncode,btnDecode; 
-//Coneccion de los botones
+let offset,mensaje_encode,resultado,btnEncode,btnDecode;
+//se tiene que declarar la variable que llama a caba ID de los textbox 
+//dentro de cada evento porque si no esta dentro cuando se haga clic no va a poder encontrarlo
+    //Coneccion de los botones
     //ENCODE
     btnEncode=document.getElementById("buttonEncode");
     btnEncode.addEventListener("click",function(){
-        cipher.encode();
+        mensaje_encode=document.getElementById("txt_encode").value;
+        offset=document.getElementById("txt_offset").value;  
+        cipher.encode(offset,mensaje_encode);
     })
+    document.getElementById("txt_resultado").innerHTML=vacio;
     //DECODE
     btnDecode=document.getElementById("buttonDecode")
     btnDecode.addEventListener("click",function(){
+        offset=document.getElementById("txt_offset").value;
         cipher.decode();
     });
-//coneccion de los campos de texto
-    offset=document.getElementById("txt_offset").value;
-    mensaje_encode=document.getElementById("txt_encode").value;
-    mensaje_decode=document.getElementById("txt_encode").value;
+
     
     
 
