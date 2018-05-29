7ac531ad27342ccafef08ef65910789e846e10d1
AlidaContreras <alidacontreras01@gmail.com>
2018-05-29 17:36:50 -0500

    Ya esta el ENCODE listo :D yyeeee...git add .git add .

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,23 @@
 window.cipher = {
     encode: (offset, string) => {
-        let resultado = "";//resultado es la variable donde se va guardar lo que queremos y definimos mas abajo
-        for (let i = 0; i < mensaje_encode.length; i++) {//este for recorre el string 
+        resultado = '';//resultado es la variable donde se va guardar lo que queremos y definimos mas abajo
+        for (let i = 0; i < string.length; i++) {//este for recorre el string 
             //con la siguiente linea obtiene el numero de posicion de cada letra  del string y lo almacena en la variable posicion
-            let posicion = mensaje_encode.charCodeAt(i);//con al charcodeat obtengo el numero de Posicion en el Ascci
+            let posicion = string.charCodeAt(i);//con al charcodeat obtengo el numero de Posicion en el Ascci
             //aqui ya tengo el numero de Ascci; y con las condicionales veo que caso es MAYUSCULA, minuscula o espacios
             if (posicion > 64 && posicion <= 90) {
-                resultado += String.fromCharCode(((posicion - 65 + offset) % 26) + 65);
+                resultado += String.fromCharCode(((posicion - 65 + offset) % 26) + 65);//esta es la formula para que recorra de nuevo el abc
             }
-            else if (posicion > 96 && posicion < 123) {
-                resultado += String.fromCharCode(posicion + offset);
+            else if (posicion > 96 && posicion <= 122) {
+                resultado += String.fromCharCode(((posicion - 97 + offset) % 26) + 97);
             }
             else if (posicion == 32) {
-                resultado += " ";
+                resultado += "";
             }
-
-            document.getElementById("txt_resultado").innerHTML = resultado;
-
-        }
+        }//AQUI TERMINA EL FOR
+         return(resultado)
+    },
+    decode: (offset, string) => {
 
     }
-    //decode:()
-
 }
-
-
-
-
-function decode() {
-    console.log("hola, soy DECODE");
-
-
-}
-//cipher.encode(2,'ALIDA');
--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-let offset,mensaje_encode,resultado,btnEncode,btnDecode;
+let offset, string, resultado, btnEncode, btnDecode;
 //se tiene que declarar la variable que llama a caba ID de los textbox 
 //dentro de cada evento porque si no esta dentro cuando se haga clic no va a poder encontrarlo
-    //Coneccion de los botones
-    //ENCODE
-    btnEncode=document.getElementById("buttonEncode");
-    btnEncode.addEventListener("click",function(){
-        mensaje_encode=document.getElementById("txt_encode").value;
-        offset=parseInt(document.getElementById("txt_offset").value);  
-        cipher.encode(offset,mensaje_encode);
-    })
+//Coneccion de los botones
+//ENCODE
+btnEncode = document.getElementById("buttonEncode");
+btnEncode.addEventListener("click", function () { 
+    string = document.getElementById("txt_ingresado").value;
+    offset = parseInt(document.getElementById("txt_offset").value);
+    cipher.encode(offset, string);
     document.getElementById("txt_resultado").innerHTML=resultado;
-    //DECODE
-    btnDecode=document.getElementById("buttonDecode")
-    btnDecode.addEventListener("click",function(){
-        offset=document.getElementById("txt_offset").value;
-        cipher.decode();
-    });
-
     
-    
+})
+
+//DECODE
+btnDecode = document.getElementById("buttonDecode")
+btnDecode.addEventListener("click", function () {
+    offset = document.getElementById("txt_offset").value;
+    cipher.decode(offset, string);
+});
 
 
-    
 
-    
+
+
+
+
+
